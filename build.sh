6dc2eb137301749975b5a7a422aa7788b1e2297c
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-03 14:17:51 -0500

    SAGE-1354: remove 'Sage' reference and use latest deb-builder

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 docker run --rm \
   -e NAME="waggle-powercycle" \
   -e DESCRIPTION="Waggle Agent Power Cycle Tools" \
-  -e "MAINTAINER=sagecontinuum.org" \
   -e "DEPENDS=waggle-common-tools (>= 0.2.0)" \
   -v "$PWD:/repo" \
-  waggle/waggle-deb-builder:0.3.2
+  waggle/waggle-deb-builder:latest
